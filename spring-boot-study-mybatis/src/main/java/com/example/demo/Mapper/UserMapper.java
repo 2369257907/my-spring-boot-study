2251daa4e4d1e35f1df71a1898f94ed5763f8ca6
2369257907 <2369257907@qq.com>
2021-10-28 23:38:10 +0800

    新增springboot通过注解集成mybatis

---
+++ spring-boot-study-mybatis/src/main/java/com/example/demo/Mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package com.example.demo.Mapper;
+
+import com.example.demo.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 86171
+ */
+
+@Mapper
+@Repository
+public interface UserMapper {
+    @Select("SELECT * FROM User ")
+    ArrayList<User> findAllUser();
+}
---
+++ spring-boot-study-mybatis/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.Mapper.UserMapper;
+import com.example.demo.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @RequestMapping("/queryUserList")
+    public List<User> queryUserList(){
+        List<User> userList = new ArrayList<>();
+        userList = userMapper.findAllUser();
+        return userList;
+    }
+}
